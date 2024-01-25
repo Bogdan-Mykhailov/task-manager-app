f4dfb6916a2f2964e28e1036102569718012c25f
Bogdan_Mykhailov <bogdan_mykhailov@icloud.com>
2024-01-25 11:08:10 +0200

    :tada: update package.json

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: 'http://localhost:3001',
+  // baseURL: 'http://localhost:3001',
+  baseURL: 'https://db-task-manager.vercel.app/',
   headers: {
     'Content-Type': 'application/json',
   },
